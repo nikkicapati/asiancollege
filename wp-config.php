531b15d490a5a7410408008012778cca5cb8e404
nikkicapati <nikki.capati@gmail.com>
2014-12-06 19:59:04 +0800

    Update wp-config.php
    new connection

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'asiancollege');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'adminrh4GhGC');
 
 /** MySQL database password */
-define('DB_PASSWORD', '123pass');
+define('DB_PASSWORD', 'jcRUVG3hyyXr');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
